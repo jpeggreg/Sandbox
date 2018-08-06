2f4453acb7b05329c3dbde5d816c41c03dfb1863
Greg Taylor <gregory.taylor1@my.jcu.edu.au>
2018-08-06 10:48:53 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,9 @@
 """Greg Taylor"""
+
+MINIMUM_LENGTH = 6
+
+password = input("Enter a password with at least {} characters ".format(MINIMUM_LENGTH))
+while len(password) < MINIMUM_LENGTH:
+    password = input("Enter a password with at least {} characters ".format(MINIMUM_LENGTH))
+
+print('*' * len(password))
